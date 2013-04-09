40224fde11f070b0bb5271608458cb12a865907c
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2013-04-09 12:58:03 -0400

    modify sample shiny app

--- inst/app/server.R
+++ inst/app/server.R
@@ -3,7 +3,7 @@ shinyServer(function(input, output) {
   output$myChart <- renderChart({
     hair_eye = as.data.frame(HairEyeColor)
     p6 <- nvd3Plot(Freq ~ Hair | Eye, data = subset(hair_eye, Sex == input$gender), 
-      type = input$type, id = 'myChart')
+      type = input$type, id = 'myChart', width = 800)
     p6$chart(color = c('brown', 'blue', '#594c26', 'green'), stacked = input$stack)
     return(p6)
   })
--- inst/app/ui.R
+++ inst/app/ui.R
@@ -1,15 +1,21 @@
 require(rNVD3)
-shinyUI(bootstrapPage(
-  selectInput(inputId = "gender",
-    label = "Choose Gender",
-    choices = c("Male", "Female"),
-    selected = "Male"),
-  selectInput(inputId = "type",
-    label = "Choose Chart Type",
-    choices = c("multiBarChart", "multiBarHorizontalChart"),
-    selected = "multiBarChart"),
-  checkboxInput(inputId = "stack",
-    label = strong("Stack Bars?"),
-    value = FALSE),
-  showOutput("myChart")
+shinyUI(pageWithSidebar(
+  headerPanel("rNVD3: Interactive Charts from R using NVD3.js"),
+  
+  sidebarPanel(
+    selectInput(inputId = "gender",
+      label = "Choose Gender",
+      choices = c("Male", "Female"),
+      selected = "Male"),
+    selectInput(inputId = "type",
+      label = "Choose Chart Type",
+      choices = c("multiBarChart", "multiBarHorizontalChart"),
+      selected = "multiBarChart"),
+    checkboxInput(inputId = "stack",
+      label = strong("Stack Bars?"),
+      value = FALSE)
+  ),
+  mainPanel(
+    showOutput("myChart")
+  )
 ))
